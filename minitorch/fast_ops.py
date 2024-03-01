86a38d2a72ab1c136d0e959be19595f8a840d9ff
daniel <1534513+dantp-ai@users.noreply.github.com>
2024-03-01 10:03:30 +0100

    Ignore unused vars for assignments yet to be implemented

--- minitorch/fast_ops.py
+++ minitorch/fast_ops.py
@@ -281,8 +281,8 @@ def _tensor_matrix_multiply(
     Returns:
         None : Fills in `out`
     """
-    a_batch_stride = a_strides[0] if a_shape[0] > 1 else 0
-    b_batch_stride = b_strides[0] if b_shape[0] > 1 else 0
+    a_batch_stride = a_strides[0] if a_shape[0] > 1 else 0  # noqa: F841
+    b_batch_stride = b_strides[0] if b_shape[0] > 1 else 0  # noqa: F841
 
     # TODO: Implement for Task 3.2.
     raise NotImplementedError("Need to implement for Task 3.2")
